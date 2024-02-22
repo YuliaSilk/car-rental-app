202be9a9e92a20c71135bc674593d3a732043ef6
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-02-22 15:20:56 +0200

    start create app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+export const App = () => {
+  return (
+    <div>
+
+
+    </div>
+  );
+};
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { App } from 'App';
+import { GlobalStyle } from 'GlobalStyles'; 
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    main: '#3470FF',
+    header: '#153834',
+    backgraund: '#fff',
+    active:'#c70e38',
+    text:'#232323',
+    input:'#edb234',
+    acent:'#0B44CD',
+  },
+  spacing: value => `${value*4}px`,
+  radius: {
+    m: '12px',
+  },
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    <GlobalStyle/>
+    </ThemeProvider>
   </React.StrictMode>
 );
